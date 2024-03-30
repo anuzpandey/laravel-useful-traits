64b48e4d82c1d94188221924a0a8675a8907e4af
anuzpandey <anuzpandey@users.noreply.github.com>
2024-03-30 19:04:56 +0000

    Fix styling

--- src/Concerns/HasSeoColumns.php
+++ src/Concerns/HasSeoColumns.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace AnuzPandey\LaravelUsefulTraits\Concerns;
 
-use Illuminate\Database\Eloquent\Builder;
-
 trait HasSeoColumns
 {
     public function initializeHasSeoColumns(): void
